b13e5738d29e2aed427e00c9a4d4b554bcf905e2
VenkatacharanrajLV <VenkatacharanrajLV@DESKTOP-E510Q6A.in.ibm.com>
2018-05-29 17:52:53 +0530

    acad gild 1.2

---
+++ src/intro_to_java/BitAndLogicalAnd.java
@@ -0,0 +1,34 @@
+package intro_to_java;
+
+import java.util.Scanner;
+
+public class BitAndLogicalAnd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Byte a = 0, b = 0;
+
+		boolean left, right;
+
+		Scanner in = new Scanner(System.in);
+		System.out.println("enter first number");
+		a = in.nextByte();
+		System.out.println("enter second number");
+		b = in.nextByte();
+
+		if (a > 0)
+			left = true;
+		else
+			left = false;
+
+		if (b > 0)
+			right = true;
+		else
+			right = false;
+
+		System.out.println("Bitwise And of a- " + a + "and b - " + b + " is " + (a & b));
+		System.out.println("Logical And of a- " + a + "and b - " + b + " is " + (left && right));
+		in.close();
+	}
+
+}
