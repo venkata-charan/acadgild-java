7518cafa98669c5eda60e83d452a57490b4a9ccd
VenkatacharanrajLV <VenkatacharanrajLV@DESKTOP-E510Q6A.in.ibm.com>
2018-05-29 17:20:00 +0530

    acad gild 1.1

--- src/intro_to_java/AddWithOutPlus.java
+++ src/intro_to_java/AddWithOutPlus.java
@@ -6,6 +6,7 @@ public class AddWithOutPlus {
 
 	public static void main(String[] args) {
 		int a = 0, b = 0;
+		int c = 0;
 
 		Scanner in = new Scanner(System.in);
 		System.out.println("enter first number");
